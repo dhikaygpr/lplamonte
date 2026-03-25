cc4991c688e184a55157ffd16b413d06142615c6
dhikaygpr <dhika.ygpr@gmail.com>
2026-03-25 14:11:18 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // atau plugin lain jika bukan react
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lplamonte/', // Nama repo Anda harus ada di sini
 })
