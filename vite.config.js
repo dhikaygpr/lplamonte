7f83c3adc98204677cb0fc34c3fbf12019838137
dhikaygpr <dhika.ygpr@gmail.com>
2026-03-25 14:19:18 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/lplamonte/', // TAMBAHKAN BARIS INI (sesuaikan nama repo)
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/lplamonte/', // Baris ini yang akan memperbaiki Error 404
 })
